628ed2052e0764a804e05e31869625ec9263ef29
yujin.jeong <jane320@naver.com>
2025-12-22 12:20:43 +0900

    객체 지향 프로그래밍

---
+++ src/oop1/MusicPlayer.java
@@ -0,0 +1,31 @@
+package oop1;
+
+public class MusicPlayer {
+    int volume = 0;
+    boolean isOn  = false;
+
+    void on(){
+        isOn = true;
+        System.out.println("음악 플레이어 시작");
+    }
+    void off(){
+        isOn = false;
+        System.out.println("음악 플레이어 종료");
+    }
+    void volumeUp(){
+        volume++;
+        System.out.println("음악 플레이어 볼룸:" +volume);
+    }
+    void volumeDown(){
+        volume--;
+        System.out.println("음악 플레이어 볼룸:" +volume);
+    }
+    void showStatus(){
+        System.out.println("음악 플레이어 상태 확인");
+        if(isOn) {
+            System.out.println("음악 플레이어 ON, 볼륨:" + volume);
+        } else {
+            System.out.println("음악 플레이어 OFF");
+        }
+    }
+}
