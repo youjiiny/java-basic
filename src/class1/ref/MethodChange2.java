71f0ef90e398bcfa363f09bf192d8beed0b2e149
yujin.jeong <jane320@naver.com>
2025-12-22 10:13:44 +0900

    기본ã형 vs 참ㅁ조형3 - 메서드 호출

---
+++ src/class1/ref/MethodChange2.java
@@ -0,0 +1,15 @@
+package class1.ref;
+
+public class MethodChange2 {
+    public static void main(String[] args) {
+        Data dataA = new Data();
+        dataA.value = 10;
+        System.out.println("메서드 호출 전: dataA.value = " +dataA.value);
+        changePrimitive(dataA);
+        System.out.println("메서드 호출 후: dataA.value = "+ dataA.value);
+    }
+
+    static void changePrimitive(Data dataX) {
+        dataX.value = 20;
+    }
+}
