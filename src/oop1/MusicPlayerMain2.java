e363737140d3ea7108679bdea47cf8725ce6094e
yujin.jeong <jane320@naver.com>
2025-12-22 11:52:18 +0900

    절차 지향 프로그래밍2 - 데이터 묶음

---
+++ src/oop1/MusicPlayerMain2.java
@@ -0,0 +1,33 @@
+package oop1;
+
+public class MusicPlayerMain2 {
+    public static void main(String[] args) {
+        MusicPlayerData data = new MusicPlayerData();
+
+        // 음악 플레이어 켜기
+        data.isOn = true;
+        System.out.println("음악 플레이어 시작");
+
+        // 볼륨 증가
+        data.volume++;
+        System.out.println("음악 플레이어 볼룸:" +data.volume);
+        // 볼륨 증가
+        data.volume++;
+        System.out.println("음악 플레이어 볼룸:" + data.volume);
+
+        // 볼륨 감소
+       data.volume--;
+        System.out.println("음악 플레이어 볼룸:" + data.volume);
+
+        // 음악 플레이어 상태
+        System.out.println("음악 플레이어 상태 확인");
+        if(data.isOn) {
+            System.out.println("음악 플레이어 ON, 볼륨:" + data.volume);
+        } else {
+            System.out.println("음악 플레이어 OFF");
+        }
+        // 음악 플레이어 끄기
+        data.isOn = false;
+        System.out.println("음악 플레이어 종료");
+    }
+}
