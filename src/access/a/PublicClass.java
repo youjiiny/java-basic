9307e0ca9ea4e8fde74173879e21a91d48e1ce99
yujin.jeong <jane320@naver.com>
2025-12-22 16:25:35 +0900

    접근 제어자 사용 - 클래스 레벨

---
+++ src/access/a/PublicClass.java
@@ -0,0 +1,17 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefaultClass1 class1 = new DefaultClass1();
+        DefaultCalss2 class2 = new DefaultCalss2();
+
+    }
+}
+
+class DefaultClass1 {
+
+}
+class DefaultCalss2{
+
+}
