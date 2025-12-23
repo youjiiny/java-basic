051579553def81155537fcd31404aa26776670a5
yujin.jeong <jane320@naver.com>
2025-12-23 10:06:52 +0900

    static 메서드3

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,11 @@
 package static2;
 
+import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -12,5 +14,13 @@ public class DecoDataMain {
         System.out.println("3. 인스턴스 호출2");
         DecoData data2 = new DecoData();
         data2.instanceCall();
+
+        // 인스턴스를 통한 접근 방식 권장하지 않음
+        // 정적 메서드는 클래스 소속이기 때문
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        // 클래스를 통한 접근
+        staticCall();
     }
 }
