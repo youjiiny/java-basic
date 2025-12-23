dbcc06d80429fe02f42fa9ce9e841e7b5641b273
yujin.jeong <jane320@naver.com>
2025-12-23 09:35:21 +0900

    static 변수3

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -10,5 +10,14 @@ public class DataCountMain3 {
 
         Data3 data3 = new Data3("C");
         System.out.println("C count="+Data3.count);
+
+        // 인스턴스를 통한 접근
+        // 정적 변수의 경우 인스턴스를 통한 접근을 권장 X
+        // 마치 인스턴스 변수에 접근하는 것처럼 오해할 수 있기 때문
+        Data3 data4 = new Data3("D");
+        System.out.println("D count="+data4.count);
+        
+        // 클래스를 통한 접근
+        System.out.println("D count="+Data3.count);
     }
 }
