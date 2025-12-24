cc1bd1fb6abf6a9fc3556215bfb2e4b7bb6243e3
yujin.jeong <jane320@naver.com>
2025-12-24 09:39:32 +0900

    다형성 활용3

---
+++ src/poly/ex2/AnimalPolyMain2.java
@@ -0,0 +1,19 @@
+ package poly.ex2;
+
+public class AnimalPolyMain2 {
+    public static void main(String[] args) {
+        Animal[] amimalArr = {new Dog(), new Cat(), new Caw(), new Duck()};
+
+        for (Animal animal : amimalArr) {
+            soundAnimal(animal);
+        }
+
+    }
+
+    private static void soundAnimal(Animal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+}
